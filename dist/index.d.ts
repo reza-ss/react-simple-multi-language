691fae9a023ae3162d9363ca8cecdc2c4061b666
reza sedighi <rezasedighi61@gmail.com>
2021-12-21 22:13:29 +0330

    remove dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { I18nText, InternationalizationProvider, useCurrentLanguage, useSetLocale } from "./component";
-export { I18nText, InternationalizationProvider, useCurrentLanguage, useSetLocale, };
