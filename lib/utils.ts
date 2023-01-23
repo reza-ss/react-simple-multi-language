66f2c413eada417258704d8a8c8a1ab32977e76b
reza sedighi <rezasedighi61@gmail.com>
2023-01-23 08:58:52 +0330

    support nested object

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
 import { languageStore } from "./store";
 import { LanguageContextType } from "./types";
 
+const getPropertyWithKeyIdentifier = (
+  ob: {},
+  key: string
+): string | undefined =>
+  key
+    .split(".")
+    .reduce((prev, current) => prev[current as keyof typeof ob], ob as string);
+
 const cacheTranslate = () => {
   let cache: { [key: string]: string } = {};
   let translationConfig = languageStore.getState();
@@ -11,22 +19,26 @@ const cacheTranslate = () => {
   });
 
   return (id: string, params?: LanguageContextType) => {
+    if (cache[id]) return cache[id];
+
     const { currentLanguage, translations } = translationConfig;
     const messages = translations[currentLanguage];
 
     if (!messages || !Object.keys(messages)?.length) return "";
 
-    if (params && cache[id]) return cache[id];
+    const text = getPropertyWithKeyIdentifier(messages, id);
+
+    if (!text) return "";
 
     if (params) {
       cache[id] = Object.keys(params).reduce((prev, current) => {
         const regex = new RegExp(`{${current}}`, "g");
         return prev.replace(regex, params[current][currentLanguage]);
-      }, messages[id]);
+      }, text);
 
       return cache[id];
     } else {
-      return messages[id];
+      return text;
     }
   };
 };
